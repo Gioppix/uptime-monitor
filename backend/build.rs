996d4b4937b424320f21f68c9852fda0c4014a63
Giovanni <gfeltrin25@gmail.com>
2025-11-14 10:17:24 +0100

    Allow env overriding in build script

--- backend/build.rs
+++ backend/build.rs
@@ -1,15 +1,21 @@
+use std::{collections::HashMap, env};
+
 fn main() {
     println!("cargo:rerun-if-changed=.env");
 
-    // Load the .env file
+    let existing_vars: HashMap<String, String> = env::vars().collect();
+
     match dotenvy::dotenv() {
         Ok(_) => println!("cargo:warning=Successfully loaded .env file"),
         Err(e) => println!("cargo:warning=Failed to load .env file: {}", e),
     }
 
-    // Iterate through all environment variables
-    for (key, value) in std::env::vars() {
-        // Export environment variables to the build process
+    for (key, value) in env::vars() {
+        // Give precedence to existing env vars
+        let value = existing_vars.get(&key).unwrap_or(&value);
         println!("cargo:rustc-env={}={}", key, value);
+
+        // Rerun if variable changes (e.g. removing an override)
+        println!("cargo::rerun-if-env-changed={key}");
     }
 }
